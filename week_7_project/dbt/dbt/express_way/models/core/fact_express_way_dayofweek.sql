9a9f990531bac7392c003601b13f563d67439d07
booknattawat <nattawat.tr@ku.th>
2023-05-04 21:26:52 +0700

    fix typo

--- week_7_project/dbt/dbt/express_way/models/core/fact_express_way_dayofweek.sql
+++ week_7_project/dbt/dbt/express_way/models/core/fact_express_way_dayofweek.sql
@@ -5,7 +5,7 @@
 select 
   road_name,
   EXTRACT(YEAR FROM currenct_datetime) AS year,
-  EXTRACT(DAYOFWEEK FROM currenct_datetime) AS DAYOFWEEK,
+  EXTRACT(DAYOFWEEK FROM currenct_datetime) -1 AS DAYOFWEEK,
   AVG(speed) AS avg_speed,
 from {{ source('express_way','express_raw') }}
 group by 1, 2, 3
